882abfafb96fa9c9f6c89a600bf673e34cac4f55
chizhovnikita <145499117+chizhovnikita@users.noreply.github.com>
2023-09-20 21:30:20 +0300

    Add files via upload

---
+++ 3pr_1.py
@@ -0,0 +1,12 @@
+a=int(input('Введите a '))
+b=int(input('Введите b '))
+c=int(input('Введите c '))
+
+if a >= 1 and a <= 3 :
+ print(a , 'входит')
+
+if b >= 1 and b <= 3 :
+    print(b , 'входит')
+
+if c >= 1 and c <= 3 :
+ print(c , 'входит')
---
+++ pr3_3.py
@@ -0,0 +1,6 @@
+a = int(input("Введите a "))
+b = int(input("Введите b "))
+s = a + b
+l = a - b
+print("Сумма: ", s)
+print("Разность: ", l)
